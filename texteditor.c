3526a60dfb61b73375a4548e4c2c6425f91ebd5f
firley <konrad.firley@student.reutlingen-university.de>
2024-08-08 23:50:01 +0200

    added the ability to quit the programm with 'q'

--- texteditor.c
+++ texteditor.c
@@ -3,6 +3,6 @@
 //read() and STDIN_FILENO -> from unistd.h 
 int main() {
     char c;
-    while (read(STDIN_FILENO, &c, 1) == 1); //read() -> reads 1 byte and puts it into the variable c 
+    while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q'); //read() -> reads 1 byte and puts it into the variable c 
     return 0;                               // while makes it do so until there is nothing to read anymore 
 }
